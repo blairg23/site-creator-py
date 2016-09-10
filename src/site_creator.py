ebf5b997f298ea2da96576a884394d8b70409ff2
blairg23 <blairg23@gmail.com>
2016-09-10 17:41:15 -0600

    Initial commit

---
+++ src/site_creator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Name: site_creator.py
+Author: Blair Gemmer
+Version: 20160910
+
+Description: 
+
+Creates a template for a web application, based on the contents 
+of the "import/<project_name>" directory, where <project_name> is
+the name of the current project.
+
+'''
+
